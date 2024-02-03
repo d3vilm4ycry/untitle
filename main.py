a4988d64b11cf8329d1ccc233082291858f68d06
danyadesyatov <daniladesyatov@mail.ru>
2024-02-03 18:44:21 +0300

    working version

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
+password_length = int(input("Введите длинну пароля: "))
+generated_password = generate_password(password_length)
+print("Пароль:", generated_password)

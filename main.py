7f9b267926bf3572b6fe1a16266d36fbad27a9ca
danyadesyatov <daniladesyatov@mail.ru>
2024-02-03 18:39:43 +0300

    generator parolya

---
+++ main.py
@@ -0,0 +1,6 @@
+import string
+import random
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
